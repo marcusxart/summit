b18b5998c3c1098d6213a4e0cf42fa351b991901
marcusxart <cemeji64@gmail.com>
2024-05-03 08:21:01 +0100

    update

--- client/src/pages/home/components/card.jsx
+++ client/src/pages/home/components/card.jsx
@@ -2,26 +2,33 @@ import React from 'react';
 // import GalleyIcon from '../../../assets/svg/galleyIcon';
 // import CameraIcon from '../../../assets/svg/cameraIcon';
 
-const Card = ({ title }) => {
+const Card = ({ title, points, img }) => {
   return (
-    <div className="w-full max-w-[372px] bg-white p-[24px] text-center shadow-lg rounded-[10px]">
-      <div className="max-w-[300px] mx-auto">
-        <h2 className=" uppercase  text-[32px] leading-[40px] text-[#0B4C7A] font-bold">
-          {title}
-        </h2>
-        <p className="font-bold text-[16px] uppercase text-[#508DC5] my-[16px] ">
-          10 points
-        </p>
+    <div className="w-full max-w-[372px] bg-white p-[24px] text-center shadow-lg rounded-[10px] flex">
+      <div className=" flex-1 max-w-[300px] mx-auto flex flex-col justify-between w-full">
+        <div>
+          <h2 className=" uppercase  text-[32px] leading-[40px] text-[#0B4C7A] font-bold">
+            {title}
+          </h2>
+          <p className="font-bold text-[16px] uppercase text-[#508DC5] my-[16px] ">
+            {points}
+          </p>
+          <p className="text-[14px] font-medium">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur
+            cupi
+          </p>
+        </div>
 
-        <p className="text-[14px] font-medium">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur cupi
-        </p>
-        {/* <div className="grid place-items-center my-6">
-          <GalleyIcon />
-        </div> */}
-        <button className="w-full bg-[#4EA191] text-white font-bold capitalize h-[48px] rounded-[80px] mt-[16px]">
-          Take picture
-        </button>
+        <div className=" h-fit">
+          {img && (
+            <div className="w-full h-[120px] mt-[16px]">
+              <img src={img} alt="" className="w-full h-full object-contain" />
+            </div>
+          )}
+          <button className="w-full bg-[#4EA191] text-white font-bold capitalize h-[48px] rounded-[80px] mt-[16px]">
+            Take picture
+          </button>
+        </div>
       </div>
     </div>
   );
--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -2,9 +2,53 @@ import React from 'react';
 import Card from './components/card';
 import PowerIcon from '../../assets/svg/powerIcon';
 // import Logo from '../../assets/svg/logo';
+import blueM from './../../assets/images/blue-mountain.jpeg';
+import ice from './../../assets/images/ice-cream.jpg';
 
 const Home = () => {
-  const list = [1, 2, 3, 4, 5, 6, 7, 8];
+  const list = [
+    {
+      title: 'Welcome to blue Mountain quest!',
+      point: 'ready to start',
+      img: blueM,
+    },
+    {
+      title: 'Clue 1',
+      point: '10 point',
+    },
+    {
+      title: 'Clue 2',
+      point: '10 point',
+      img: ice,
+    },
+    {
+      title: 'Clue 3',
+      point: '10 point',
+    },
+    {
+      title: 'Clue 4',
+      point: '10 point',
+
+      img: ice,
+    },
+    {
+      title: 'Clue 5',
+      point: '10 point',
+    },
+    {
+      title: 'Clue 6',
+      point: '10 point',
+    },
+    {
+      title: 'Clue 7',
+      point: '10 point',
+      img: ice,
+    },
+    {
+      title: 'Clue 8',
+      point: '10 point',
+    },
+  ];
   return (
     <div className="bg-[#124A7D]">
       <div className="max-w-[1440px] mx-auto px-[24px] lg:px-[80px] min-h-[100dvh]  pb-[100px]">
@@ -16,7 +60,12 @@ const Home = () => {
         </div>
         <div className=" grid  grid-container--fit gap-[50px] justify-center  xl:justify-between">
           {list?.map((item, idx) => (
-            <Card key={idx} title={`Clue ${item}`} />
+            <Card
+              key={idx}
+              title={item.title}
+              points={item.point}
+              img={item.img}
+            />
           ))}
         </div>
       </div>
