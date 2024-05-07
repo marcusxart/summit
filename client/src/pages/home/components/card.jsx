ad11ba9ebfcaf4b23fa3dd14523b44f6a32b9f21
marcusxart <cemeji64@gmail.com>
2024-05-07 01:08:20 +0100

    camera

--- client/src/pages/home/components/card.jsx
+++ client/src/pages/home/components/card.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 // import GalleyIcon from '../../../assets/svg/galleyIcon';
 // import CameraIcon from '../../../assets/svg/cameraIcon';
 
 const Card = ({ title, points, img }) => {
+  const [photoData, setPhotoData] = useState(null);
+  const takePicture = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const track = stream.getVideoTracks()[0];
+      const imageCapture = new ImageCapture(track);
+      const blob = await imageCapture.takePhoto();
+      const imageUrl = URL.createObjectURL(blob);
+      setPhotoData(imageUrl);
+      track.stop(); // Stop the video track to release camera resources
+    } catch (error) {
+      console.error('Error accessing camera:', error);
+    }
+  };
+  console.log(photoData);
   return (
     <div className="w-full max-w-[372px] bg-white p-[24px] text-center shadow-lg rounded-[10px] flex">
       <div className=" flex-1 max-w-[300px] mx-auto flex flex-col justify-between w-full">
@@ -25,7 +41,10 @@ const Card = ({ title, points, img }) => {
               <img src={img} alt="" className="w-full h-full object-contain" />
             </div>
           )}
-          <button className="w-full bg-[#4EA191] text-white font-bold capitalize h-[48px] rounded-[80px] mt-[16px]">
+          <button
+            className="w-full bg-[#4EA191] text-white font-bold capitalize h-[48px] rounded-[80px] mt-[16px]"
+            onClick={takePicture}
+          >
             Take picture
           </button>
         </div>
